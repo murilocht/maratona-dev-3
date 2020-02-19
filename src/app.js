fe1cead5abbe0bd0218de72c6420455f799316a4
murilocht <murilo980ti@outlook.com>
2020-02-19 12:26:08 -0300

    terminando maratonadev 3a edição

--- src/app.js
+++ src/app.js
@@ -1,16 +1,45 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const mysql = require("mysql");
+const config = require("./config/database");
 
 const server = express();
-
 server.use(express.static("src/public"));
+server.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure("src/public", {
-  express: server
+  express: server,
+  noCache: true
 });
 
+const pool = mysql.createConnection(config);
+
 server.get("/", (req, res) => {
-  res.render("index.html");
+  pool.query(`SELECT * FROM donors`, (err, result) => {
+    if (err) return res.send(err);
+
+    return res.render("index.njk", { donors: result });
+  });
+});
+
+server.post("/", (req, res) => {
+  const { name, email, blood } = req.body;
+
+  if (name == "" || email == "" || blood == "") {
+    res.send("Todos os campos são obrigatórios!");
+  }
+
+  pool.query(
+    `INSERT INTO donors (name, email, blood) VALUES ('${name}', '${email}', '${blood}')`,
+    err => {
+      if (err) {
+        console.log(err);
+        return res.send("Erro ao salvar no banco de dados!");
+      }
+
+      return res.redirect("/");
+    }
+  );
 });
 
 server.listen(3333, () => {
---
+++ src/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: "localhost",
+  user: "murilocht",
+  password: "123456",
+  database: "teste"
+};
